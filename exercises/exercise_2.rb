51202e50e79cbbf9693a8bd68d7245d14035e871
Jyles02 <Jyles999@gmail.com>
2024-02-06 18:36:19 +0800

    exercise_2.rb

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,15 @@
-age = 12
-legalage = 18
-if age >= legalage
-  print "Adult"
-else
-  print "Not Adult"
+
+
+def check_adult(age)
+  legalage = 18
+  print (age)
+
+  if age >= legalage
+    print "Adult\n"
+  else
+    print "Not Adult\n"
+  end
 end
+
+check_adult(12)
+check_adult(89)
