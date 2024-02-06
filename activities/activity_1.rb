ae44dd8d206df13ae736acefc89e66fe9e455764
Jyles02 <Jyles999@gmail.com>
2024-02-06 19:27:54 +0800

    activities/activity_1.rb

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,20 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+ages = [12,32,53,65,21,16]
+def getadult(ages)
+  legalage = 18
+  adults = Array.new
+  ages.each do |age|
+    if age >= legalage
+      adults.push(age)
+    end
+  end
+  adults.sum/ adults.size
+end
+p getadult(ages)
+
+
+
+
